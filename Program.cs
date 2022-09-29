b38d5821ce0e290256425382084cffbac92ae149
praveen dodla <dodlapraveen09@gmail.com>
2022-09-29 20:20:21 +0530

    CreateDataTableAndColumns

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AddressBookLINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //UC 2
+            AddressDataBook adb = new AddressDataBook();
+            adb.AddData();
+        }
+    }
+}
