1dad952e01885038bf55fd1370258ce7b4c3a34f
praveen dodla <dodlapraveen09@gmail.com>
2022-09-29 20:22:54 +0530

    InsertNewRecord

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Data;
+
+namespace AddressBookLINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //UC 2
+            AddressDataBook adb = new AddressDataBook();
+            DataTable table = adb.AddData();
+            //UC 3
+            adb.DisplayAddressBook(table);
+        }
+    }
+}
